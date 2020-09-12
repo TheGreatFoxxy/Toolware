2e60ab8fb32dbfacb5a7a6d14892760dd257cf2b
TheGreatFoxxy <56609591+TheGreatFoxxy@users.noreply.github.com>
2020-09-12 08:14:59 -0500

    Add Plus v1.0.0

---
+++ toolware-plus.js
@@ -0,0 +1,37 @@
+/* Toolware+
+*  plus JavaScript
+*  Written 9/11/2020
+*  Current version: v0.0.1
+*/
+function blueModeFunction() {
+    var element = document.body;
+    element.classList.toggle("blue-mode");
+}
+function redModeFunction() {
+    var element = document.body;
+    element.classList.toggle("red-mode");
+}
+function greenModeFunction() {
+    var element = document.body;
+    element.classList.toggle("green-mode");
+}
+function darkModeFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function brightModeFunction() {
+    var element = document.body;
+    element.classList.toggle("bright-mode");
+}
+function terminalModeFunction() {
+    var element = document.body;
+    element.classList.toggle("terminal-mode");
+}
+function orangeModeFunction() {
+    var element = document.body;
+    element.classList.toggle("orange-mode");
+}
+function sansserifTextFunction() {
+    var element = document.body;
+    element.classList.toggle("sans-serif-text");
+}
